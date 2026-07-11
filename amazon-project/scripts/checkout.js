dc0357e57a0f7c39d41c9c8f3ea13561d35945a6
Vedv2046 <vharambleved@gmail.com>
2026-07-11 18:25:30 +0530

    learn promises and convert code to promises

--- amazon-project/scripts/checkout.js
+++ amazon-project/scripts/checkout.js
@@ -2,11 +2,33 @@ import { renderCheckoutHeader } from "./checkout/checkoutHeader.js?v=20260708-1"
 import { renderOrderSummary } from "./checkout/orderSummary.js?v=20260708-1";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js?v=20260708-1";
 import {loadProducts} from './products.js?v=20260708-1';
+import {loadCart} from './cart.js';
 //import '../scripts/cart-class.js';
 // import '../scripts/backend-practise.js';
 
-loadProducts(() => {
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve('value1');
+        });
+    }),
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    })
+]).then((values) => {
     renderCheckoutHeader();
     renderOrderSummary();
     renderPaymentSummary();
 });
+
+/*
+loadProducts(() => {
+    loadCart(() => {
+        renderCheckoutHeader();
+        renderOrderSummary();
+        renderPaymentSummary();
+    });
+});
+*/
