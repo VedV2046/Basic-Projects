0d676f666eb68e09a8a9a9368cae05e325c9da65
Vedv2046 <vharambleved@gmail.com>
2026-07-11 19:09:53 +0530

    convert loadProducts from xhr to fetch

--- amazon-project/scripts/checkout.js
+++ amazon-project/scripts/checkout.js
@@ -1,17 +1,13 @@
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js?v=20260708-1";
 import { renderOrderSummary } from "./checkout/orderSummary.js?v=20260708-1";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js?v=20260708-1";
-import {loadProducts} from './products.js?v=20260708-1';
+import {loadProducts, loadProductsFetch} from './products.js?v=20260708-1';
 import {loadCart} from './cart.js';
 //import '../scripts/cart-class.js';
 // import '../scripts/backend-practise.js';
 
 Promise.all([
-    new Promise((resolve) => {
-        loadProducts(() => {
-            resolve('value1');
-        });
-    }),
+    loadProductsFetch(),
     new Promise((resolve) => {
         loadCart(() => {
             resolve();
